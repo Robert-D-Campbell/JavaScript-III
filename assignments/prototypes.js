2ac0c182d195face9bfe43aa34086b89231f7261
Robert Campbell <robertc1191@Roberts-MacBook-Pro-2.local>
2019-09-18 14:52:46 -0700

    stretch task completed

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -61,6 +61,21 @@ Humanoid.prototype.greet = function() {
   return `${this.name} offers a greeting in ${this.language}`;
 };
 
+function Villain(evilAttr) {
+  Humanoid.call(this, evilAttr);
+}
+Villain.prototype = Object.create(Humanoid.prototype);
+Villain.prototype.pencilTrick = function() {
+  return `How about a magic trick? I'm gonna make this ${this.weapons} disappear`;
+};
+
+function Hero(heroAttr) {
+  Humanoid.call(this, heroAttr);
+}
+Hero.prototype = Object.create(Humanoid.prototype);
+Hero.prototype.batThrow = function() {
+  return `WHERE IS SHE?!?! *throws* ${this.weapons} `;
+};
 /*
  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
  * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -68,6 +83,34 @@ Humanoid.prototype.greet = function() {
  */
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
+
+const hero = new Hero({
+  createdAt: new Date(),
+  dimensions: {
+    length: 3,
+    width: 3,
+    height: 5
+  },
+  healthPoints: 20,
+  name: "Batman",
+  team: "Justice League",
+  weapons: ["Batarang"],
+  language: "Sore Throat"
+});
+
+const villain = new Villain({
+  createdAt: new Date(),
+  dimensions: {
+    length: 3,
+    width: 2,
+    height: 4
+  },
+  healthPoints: 20,
+  name: "Joker",
+  team: "Suicide Squad",
+  weapons: ["Pencil"],
+  language: "Sarcasm"
+});
 
 const mage = new Humanoid({
   createdAt: new Date(),
@@ -121,6 +164,8 @@ console.log(archer.language); // Elvish
 console.log(archer.greet()); // Lilith offers a greeting in Elvish.
 console.log(mage.takeDamage()); // Bruce took damage.
 console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+console.log(villain.pencilTrick());
+console.log(hero.batThrow());
 
 // Stretch task:
 // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.
